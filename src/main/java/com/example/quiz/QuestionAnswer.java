1dbcd436b1065c581bdde54cbc24c66352d8a5fb
Owjasweeta <108125846+Owjasweeta@users.noreply.github.com>
2023-08-12 00:49:46 +0530

    Add files via upload

---
+++ src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+
+            "The full form of DOM is?",
+            "The main memory of a computer system is?",
+            "SSL stands for:",
+            "Total number of layers in OSI model is",
+            "OCR stands for:"
+
+    };
+
+    public static String choices[][] = {
+            {"Document-oriented memory", "Document object model", "Document object memory", "None"},
+            {"Non-volatile","Volatile","Restricted","Unrestricted"},
+            {"Secure Socket Layer","Secure Secret Level","Secure System Level","Section Security Layer"},
+            {"5","7","9","11"},
+            {"Online Computer Retrieval","Optical Character Reader","Optical Card Reader","Office Cash Reader"}
+    };
+
+    public static String correctAnswer[] = {
+            "Document object model","Volatile","Secure Socket Layer","7","Optical Character Reader"
+    };
+}
